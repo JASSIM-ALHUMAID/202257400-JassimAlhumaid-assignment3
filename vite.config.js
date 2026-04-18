de96537b7bf4f7bbf3e3b33c707ee0b154de1269
Jassim ALHUMIAD <s202257400@kfupm.edu.sa>
2026-04-18 21:52:41 +0300

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/202257400-JassimAlhumaid-assignment3/',
   plugins: [
     tailwindcss(),
   ],
